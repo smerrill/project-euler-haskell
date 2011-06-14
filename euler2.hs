be17edd2e75e9bceacf2ee42e8cda76fb4910045
Steven Merrill <steven.merrill@gmail.com>
2011-06-14 15:33:05 -0700

    Much nicer lazy Haskell example.

--- euler2.hs
+++ euler2.hs
@@ -3,10 +3,9 @@
 # let fib 1 = 1
 # let fib n = fib (n-1) + fib (n-2)
 
-# But this way works without taking forever by using a lazy memoized list.
-let fib n = fibs !! n where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+# Easier way. Make the lazy list of all fibonacci numbers.
+let fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
-# This is inefficient. fib 100 is WAY too big.
-# I don't know enough about Haskell yet to take from a lazy list UNTIL x.
-sum(filter even [fib x | x <- [1..100], fib x < 4000000])
+# Duh - takeWhile. I knew that from Scala.
+sum (filter even (takeWhile (< 4000000) fibs))
 
